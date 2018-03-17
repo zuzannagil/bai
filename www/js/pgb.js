b8ef656cb57b89b5a4d3e8f6e21f8b39001d17c5
Zuzanna Gil <zuzannagil@protonmail.com>
2018-03-17 21:49:29 +0100

    Initial Commit

---
+++ www/js/pgb.js
@@ -0,0 +1,28 @@
+function init() {
+	document.addEventListener("deviceready",onDeviceReady, false);
+}
+
+function onDeviceReady() {
+	navigator.notification.beep(1);
+}
+
+function deviceInfo() {
+
+	info =  'Hi, I am your smartphone :-)' + '\n' +
+			'=====' + '\n' +
+			'Device Name    : '     + device.name     + '\n' + 
+			'Device Cordova : '  + device.cordova + '\n' + 
+			'Device Platform: ' + device.platform + '\n' + 
+			'Device UUID    : '     + device.uuid     + '\n' + 
+			'Device Model   : '    + device.model     + '\n' + 
+			'Device Version : '  + device.version  + '\n';
+
+function personalInfo() {
+
+	info =  'Name : Zuzanna Gil'  + '\n' + 
+			'Phone number : 501 225 774' + '\n' + 
+			'E-mail address : zuzannagil@wp.pl' + device.platform + '\n' + 
+			
+	navigator.notification.alert(info);
+	
+}
